7f058b6f766cc6ca98d879600e2f539251dbeb87
Antoine M <antoine.montaut@gmail.com>
2018-04-03 15:53:06 +0200

    memory implementation

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def main():
     print('Welcome to Ant-1!')
     game = Game()
     # game.run()
-    game.run_episodes(num_episodes=10, num_frames=1e4)
+    game.run_episodes(num_episodes=10, num_frames=5e4)
 
 if __name__ == '__main__':
     main()
